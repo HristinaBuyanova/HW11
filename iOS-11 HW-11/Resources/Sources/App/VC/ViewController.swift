c1718f0f6533aa20e8823c0981e44db638843edf
HristinaBuyanova <hristina-a@mail.ru>
2023-09-20 21:36:24 +0300

    Add // MARK: -

--- iOS-11 HW-11/Resources/Sources/App/VC/ViewController.swift
+++ iOS-11 HW-11/Resources/Sources/App/VC/ViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+    // MARK: - Setup
+
+    private func setupHierrarchy () {
+
+    }
+
+    private func setupLayout () {
+
+    }
+
+
+    // MARK: - Actions
 
 
 }
